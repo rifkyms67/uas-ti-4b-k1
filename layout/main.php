9a2d492d0ff0f19632b1f06a77d2a22d6752fe2a
akokose <rikis395@gmail.com>
2024-07-03 20:07:48 +0700

    initialize navigation bar / menu

---
+++ layout/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pinjam Buku</title>
+  <link rel="stylesheet" href="./public/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+    <?php
+      include 'layout/navbar.php'
+    ?>
+</body>
+</html>
---
+++ layout/navbar.php
@@ -0,0 +1,17 @@
+<nav>
+  <div class="navbar">
+    <div class="logo"><a href="#">Pinjam Buku</a></div>
+    <ul class="menu">
+      <li><a href="#beranda">Beranda</a></li>
+      <li><a href="#">Anggota</a></li>
+      <li><a href="#">Buku</a></li>
+      <li><a href="#">T.Pinjam</a></li>
+      <li><a href="#">T.Pengembalian</a></li>
+      <li><a href="#">Resume Dashboard</a></li>
+    </ul>
+  </div>
+</nav>
+<section id="beranda">Beranda</section>
+<div class="button">
+  <a href="#beranda"><i class="fas fa-arrow-up"></i></a>
+</div>
