6893758c721d6673cebecffae9a1511520028324
rifkyms67 <rifkyms67@gmail.com>
2024-07-16 09:27:58 +0700

    add user controller

---
+++ controllers/userController.php
@@ -0,0 +1,13 @@
+<?php
+function borrowBook() {
+    require_once 'views/shared/header.php';
+    require_once 'views/user/borrowBook.php';
+    require_once 'views/shared/footer.php';
+}
+
+function updateProfile() {
+    require_once 'views/shared/header.php';
+    require_once 'views/user/updateProfile.php';
+    require_once 'views/shared/footer.php';
+}
+?>
